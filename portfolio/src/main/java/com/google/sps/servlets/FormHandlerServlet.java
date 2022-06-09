8ad822d759bfd32401bd81d6e8dada22e076ae1f
Raymond Feckoury <rjf21434@uga.edu>
2022-06-09 19:23:30 +0000

    Handle the form data and send it to the server logs using the Java servlet.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -12,68 +12,15 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
+    // Get the values entered in the form.
+    String emailValue = request.getParameter("email-input");
     String textValue = request.getParameter("text-input");
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    // Print the values in the server logs.
+    System.out.println("Email: " + emailValue);
+    System.out.println("Message: " + textValue);
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
     response.sendRedirect("http://rfeckoury-sps-summer22.appspot.com");
   }
   
 }
-
-/*
-
-import java.io.IOException;
-import java.util.Arrays;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/** Servlet that processes text. 
-@WebServlet("/text")
-public final class TextProcessorServlet extends HttpServlet {
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the input from the form.
-    String text = getParameter(request, "text-input", "");
-    boolean upperCase = Boolean.parseBoolean(getParameter(request, "upper-case", "false"));
-    boolean sort = Boolean.parseBoolean(getParameter(request, "sort", "false"));
-
-    // Convert the text to upper case.
-    if (upperCase) {
-      text = text.toUpperCase();
-    }
-
-    // Break the text into individual words.
-    String[] words = text.split("\\s*,\\s*");
-
-    // Sort the words.
-    if (sort) {
-      Arrays.sort(words);
-    }
-
-    // Respond with the result.
-    response.setContentType("text/html;");
-    response.getWriter().println(Arrays.toString(words));
-  }
-
-  /**
-   * @return the request parameter, or the default value if the parameter
-   *         was not specified by the client
-   
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    if (value == null) {
-      return defaultValue;
-    }
-    return value;
-  }
-}
-
-*/
