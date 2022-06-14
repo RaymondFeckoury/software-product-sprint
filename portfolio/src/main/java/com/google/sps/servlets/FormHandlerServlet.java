0a3f744220235d72024813a140e06cc1846e4906
Raymond Feckoury <rjf21434@uga.edu>
2022-06-14 20:13:21 +0000

    Modify the form handler servlet to store the data as an entity in Datastore.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,5 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +24,16 @@ public class FormHandlerServlet extends HttpServlet {
     // Print the values in the server logs.
     System.out.println("Email: " + emailValue);
     System.out.println("Message: " + textValue);
+
+    // Uses datastore to store all submitted information
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Contace Me Form");
+    FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("email", emailValue)
+            .set("message", textValue)
+            .build();
+    datastore.put(taskEntity);
 
     response.sendRedirect("http://rfeckoury-sps-summer22.appspot.com");
   }
